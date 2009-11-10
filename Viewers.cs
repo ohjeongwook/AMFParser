9659071c60bf2b7aef775f5080b81441541afc50
ohjeongwook <none@none.com>
2009-11-10 08:35:39 +0000

    Initial Check-in

---
+++ Viewers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Viewers
+{
+    public partial class TXTEditor : UserControl
+    {
+        public string Data
+        {
+            get
+            {
+                return this.AMFEditorRichTextBox.Text;
+            }
+            set
+            {
+                this.AMFEditorRichTextBox.Text = value;
+            }
+        }
+
+        public TXTEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
